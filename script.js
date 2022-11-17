8f0fb5cc46b8ef3cf8df573d76a4670fcfd21eba
Harry Watkins <harry.jw@outlook.com>
2022-11-17 10:18:23 +0000

    Add addBookToLibrary function

--- script.js
+++ script.js
@@ -2,4 +2,32 @@
 
     "use strict"
 
+    let myLibrary = [
+        
+    ];
+
+    class Book{
+        constructor(
+            title = "Unknown",
+            author = "Unknown",
+            pages = 0,
+            haveRead = false
+        ) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.haveRead = haveRead;
+        }
+    }
+
+    function addBookToLibrary(newBook) {
+        const book = [newBook.title, newBook.author, newBook.pages, newBook.haveRead];
+        myLibrary.push(book);
+    }
+
+    const harryPotter = new Book ("Harry Potter", "J.K Rowling", 400, true);
+    addBookToLibrary(harryPotter);
+    
+    
+
 })()
